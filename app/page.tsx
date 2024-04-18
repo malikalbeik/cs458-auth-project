10935add8e0f21ec4529d83cf057430ac27d3e04
ilker <57532341+ilkerozgen@users.noreply.github.com>
2024-04-18 16:22:31 +0300

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 'use client'
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 
 export default function Home() {
   const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push('https://ilkerozgen.github.io/cs458-project-3/');
+    }
+  }, [session, router]);
 
   return (
     <main>
-      {session?.user ? (
-        <div>
-          <h1>Welcome, you are now signed in</h1>
-          <p>Click <a href="https://github.com/ilkerozgen/cs458-project-3">here</a> to visit our deployed website.</p>
-        </div>
-      ) : (
-        <h1>Loading...</h1>
-      )}
+      <h1>Welcome, you are now signed in</h1>
     </main>
   );
 }
